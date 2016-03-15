2964db007425b42e7db7fb176868b337405ee20e
Radomir Wojtera <radomir.wojtera@gmail.com>
2016-03-15 18:36:51 +0100

    Server is ready

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var apiRouter = express.Router();
+var itemRouter = express.Router({
+    mergeParams: true
+});
+
+var itemsRepo = require('./lib/items');
+
+apiRouter.use('/items', itemRouter);
+
+apiRouter.route('/')
+    .get(function (req, res) {
+        res.status(200)
+            .send('hello, this is API');
+    });
+
+itemRouter.route('/')
+    .get(function (req, res) {
+        res.status(200)
+            .jsonp(itemsRepo(req.params.limit, req.params.offset));
+    });
+
+itemRouter.route('/:itemId')
+    .get(function (req, res) {
+        res.status(200)
+            .jsonp(itemsRepo(1, req.params.itemId)[0]);
+    });
+
+app.use('/api', apiRouter);
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {});
